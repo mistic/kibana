5d40f42e9805ee06a1316dee5316071dee39c04e
Tiago Costa <tiagoffcc@hotmail.com>
2018-08-22 01:23:44 +0100

    chore(NA): run another static modules poc with nexe-fs.

--- src/dev/build/tasks/create_static_fs_task.js
+++ src/dev/build/tasks/create_static_fs_task.js
@@ -17,93 +17,116 @@
  * under the License.
  */
 
-import { deleteAll } from '../lib';
+// import { deleteAll } from '../lib';
 import { dirname, relative } from 'path';
-import { StaticFilesystemCreator } from 'static-fs/dist/main';
-import { calculateHash, isFile, mkdir, writeFile, copyFile, readFile } from 'static-fs/dist/lib/common';
+import { isFile, writeFile, copyFile, readFile, readdir, stat } from 'static-fs/dist/lib/common';
+import { Bundle } from 'nexe-fs';
+import { createWriteStream } from 'fs';
 
 export const CreateStaticFilesystem = {
   description:
     'Creating StaticModulesFs and patching entryPoints',
 
   async run(config, log, build) {
-    const createLoaderFile = async (filePath) => {
-      const sourceFile = require.resolve(`static-fs/dist/lib/static-loader`);
-      await copyFile(sourceFile, filePath);
-    };
-
-    const patchEntryPoints = async (entryPoints, staticModulesLoader, staticModulesFs) => {
+    const patchEntryPoints = async (entryPoints, staticModulesBootstrap, staticModulesPatch, staticModulesIndex, staticModulesFs) => {
       for (const each of entryPoints) {
         const entrypoint = require.resolve(build.resolvePath(each));
 
         if (await isFile(entrypoint)) {
-          let loaderPath = relative(dirname(entrypoint), staticModulesLoader).replace(/\\/g, '/');
-          if (loaderPath.charAt(0) !== '.') {
-            loaderPath = `./${loaderPath}`;
-          }
+          let bootstrapPath = relative(dirname(entrypoint), staticModulesBootstrap).replace(/\\/g, '/');
+          let patchPath = relative(dirname(entrypoint), staticModulesPatch).replace(/\\/g, '/');
+          let indexPath = relative(dirname(entrypoint), staticModulesIndex).replace(/\\/g, '/');
           let fsPath = relative(dirname(entrypoint), staticModulesFs).replace(/\\/g, '/');
-          fsPath = `\${__dirname }/${fsPath}`;
-          let content = await readFile(entrypoint, { encoding: 'utf8' });
-          const patchLine = `require('${loaderPath}').load(require.resolve(\`${fsPath}\`));\n`;
-          let prefix = '';
-          if (content.indexOf(patchLine) === -1) {
-            const rx = /^#!.*$/gm.exec(content.toString());
-            if (rx && rx.index === 0) {
-              prefix = `${rx[0]}\n`;
-              // remove prefix
-              content = content.replace(prefix, '');
-            }
-            // strip existing loader
-            content = content.replace(/^require.*static-loader.js.*$/gm, '');
-            content = content.replace(/\/\/ load static module: .*$/gm, '');
-            content = content.trim();
-            content = `${prefix}// load static module: ${fsPath}\n${patchLine}\n${content}`;
-
-            await writeFile(entrypoint, content);
+          if (bootstrapPath.charAt(0) !== '.') {
+            bootstrapPath = `./${bootstrapPath}`;
           }
+          if (patchPath.charAt(0) !== '.') {
+            patchPath = `./${patchPath}`;
+          }
+          if (indexPath.charAt(0) !== '.') {
+            indexPath = `./${indexPath}`;
+          }
+          if (fsPath.charAt(0) !== '.') {
+            fsPath = `./${fsPath}`;
+          }
+
+          let content = await readFile(entrypoint, { encoding: 'utf8' });
+          const bootstrapLine = `require('${bootstrapPath}');`;
+          const patchLine = `const patch = require('${patchPath}');`;
+          const indexLine = `const fsIndex = JSON.parse(require('fs').readFileSync(require.resolve('${indexPath}')));`;
+          const shimLine = `patch.shimFs({
+            blobPath: require.resolve('${fsPath}'),
+            resources: fsIndex.index,
+            layout: {
+              stat: require('fs').statSync(require.resolve('${indexPath}')),
+              resourceStart: 0
+            }
+          });`;
+
+          content = `${bootstrapLine}\n${patchLine}\n${indexLine}\n${shimLine}\n${content}`;
+          await writeFile(entrypoint, content);
         }
       }
     };
 
-    const addModulesToStaticModulesFs = async (nodeModulesDir, staticModulesFs, hash) => {
-      const sf = new StaticFilesystemCreator();
+    const addAllFilesFromFolder = async (bundle, source, target) => {
+      const files = await readdir(source);
+      const all = [];
 
-      await sf.addFolder(nodeModulesDir, '/node_modules');
-      await mkdir(dirname(staticModulesFs));
-      await sf.write(staticModulesFs, hash);
+      for (const file of files) {
+        // compute the path names
+        const sourcePath = `${source}/${file}`;
+        const targetPath = `${target}/${file}`;
+
+        // is this a directory
+        const ss = await stat(sourcePath);
+        if (ss.isDirectory()) {
+          all.push(addAllFilesFromFolder(bundle, sourcePath, targetPath));
+        } else {
+          await bundle.addResource(sourcePath);
+        }
+      }
+      // wait for children to finish
+      await Promise.all(all);
     };
 
     const generateServerBundle = async () => {
       const nodeModulesDir = build.resolvePath('node_modules');
       const staticModulesDir = build.resolvePath('static_modules');
       const staticModulesFs = build.resolvePath(staticModulesDir, 'static_modules.fs');
-      const staticModulesLoader = build.resolvePath(staticModulesDir, 'static_loader.js');
+      const staticModulesIndex = build.resolvePath(staticModulesDir, 'static_modules.index');
+      const staticModulesBootstrap = build.resolvePath(staticModulesDir, 'static_bootstrap.js');
+      const staticModulesPatch = build.resolvePath(staticModulesDir, 'static_patch.js');
       const entryPointsToPatch = [
         'src/setup_node_env/babel_register'
       ];
 
-      const hash = calculateHash({
-        staticModulesDir,
-        staticModulesFs,
-        staticModulesLoader,
-        entryPointsToPatch
-      });
+      // 1st copy loader and patch
+      const sourceFile = require.resolve(`nexe-fs/bootstrap`);
+      await copyFile(sourceFile, staticModulesBootstrap);
+      const sourceFile2 = require.resolve(`nexe-fs/patch`);
+      await copyFile(sourceFile2, staticModulesPatch);
 
-      await createLoaderFile(staticModulesLoader);
-      await patchEntryPoints(entryPointsToPatch, staticModulesLoader, staticModulesFs);
-      await addModulesToStaticModulesFs(nodeModulesDir, staticModulesFs, hash);
+      // 2nd patch entrypoints
+      await patchEntryPoints(entryPointsToPatch, staticModulesBootstrap, staticModulesPatch, staticModulesIndex, staticModulesFs);
+
+      // 3rd create and load static fs
+      const bundle = new Bundle({ cwd: '/' });
+      await addAllFilesFromFolder(bundle, nodeModulesDir, 'node_modules');
+      bundle.toStream().pipe(createWriteStream(staticModulesFs));
+      await writeFile(staticModulesIndex, JSON.stringify(bundle));
     };
 
     await generateServerBundle();
 
-    // Delete node_modules folder
-    const nodeModulesDir = build.resolvePath('node_modules');
-
-    await deleteAll(
-      log,
-      [
-        `${nodeModulesDir}/**`
-      ]
-    );
+    // // Delete node_modules folder
+    // const nodeModulesDir = build.resolvePath('node_modules');
+    //
+    // await deleteAll(
+    //   log,
+    //   [
+    //     `${nodeModulesDir}/**`
+    //   ]
+    // );
   }
 };
